17e51b86502e366d921de1c7a7b502e769556fff
hau <ngtranhau@gmail.com>
2020-10-19 15:46:32 +0700

    feat: convert with argument

---
+++ cmd/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	args := os.Args
+	if len(args) != 2 {
+		fmt.Println(`go run main.go "This is a sentence"`)
+		return
+	}
+
+	text := args[1]
+	text = strings.TrimSpace(text)
+	text = strings.ToLower(text)
+	text = strings.ReplaceAll(text, " ", "_")
+
+	fmt.Println(text)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/haunt98/converter
+
+go 1.15
